def1283f1e48eb90bc71fada4a179a584646630c
badomanut <letiktikbeta@gmail.com>
2025-06-20 14:40:15 +0700

    Add Demo Observe

---
+++ Assets/Design Pattern Scripts/Observe Scripts/Observe.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observe : MonoBehaviour
+{
+    static Dictionary<string, List<Action<object[]>>> Listeners = new Dictionary<string, List<Action<object[]>>>();
+
+    public static void AddObserve(string name, Action<object[]> callback)
+    {
+        if (!Listeners.ContainsKey(name))
+        {
+            Listeners.Add(name, new List<Action<object[]>>());
+        }
+        Listeners[name].Add(callback);
+    }
+
+    public static void RemoveListener(string name, Action<object[]> callback)
+    {
+        if (!Listeners.ContainsKey(name))
+        {
+            return;
+        }
+        Listeners[name].Remove(callback);
+    }
+
+    public static void Notify(string name, params object[] datas)
+    {
+        if (Listeners.ContainsKey(name))
+        {
+            return;
+        }
+        foreach (var item in Listeners[name])
+        {
+            try
+            {
+                item?.Invoke(datas);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Error on invoke: " + e);
+            }
+            
+        }
+    }
+}
+
+
